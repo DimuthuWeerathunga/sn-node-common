515707ba95238208de9e5c8d5ad5031ed4f79a8a
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-07-03 02:29:09 +0530

    not found error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Errors
 export * from './errors/database-connection-error';
 export * from './errors/bad-request-error';
+export * from './errors/not-found-error';
 
 // Middlewares
 export * from './middlewares/error-handler';
